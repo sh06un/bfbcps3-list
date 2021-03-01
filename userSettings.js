69a5007d8b8c0d9df75c78656191e978bee38174
Jake Millington <neon_inferno@hotmail.com>
2021-03-01 18:21:28 +1100

    Add working code

---
+++ userSettings.js
@@ -0,0 +1,20 @@
+const userSettings = {
+  chosenFields: [
+    "location",
+    "mode",
+    "map",
+    "player_count",
+    "playgroup",
+    "balance"
+  ],
+  filters: {
+    rankedOnly: true,
+    openServersOnly: true,
+  },
+  sortBy: {
+    field: "player_count",
+    order: "asc",
+  }
+};
+
+export default userSettings;
